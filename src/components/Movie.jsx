6a25b72be05fcf86f38ff72de72da3a1d586025b
Vishal <vishaldhayarkar2002@gmail.com>
2025-02-13 17:46:13 +0530

    m

--- src/components/Movie.jsx
+++ src/components/Movie.jsx
@@ -2,37 +2,19 @@ import React from "react";
 import "../App.css";
 import { useNavigate } from "react-router-dom";
 
-const Movie = (props) => {
-  const { movies } = props;
+const Movie = ({ movies }) => {
   const navigate = useNavigate();
-
-  const onTitleclick = (movieId) => {
-    navigate(`/movie/${movieId}`); // Correct route
-  };
 
   return (
     <div className="MovieList">
-      {movies.map((movie) => {
-        return (
-          <div className="movie" key={movie.id} onClick={() => onTitleclick(movie.id)}>
-            <div className="image-container">
-              <img
-                src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
-                alt={movie.title}
-              />
-            </div>
-
-            <div  className="movie-details">
-              <h1 className="movie-title">{movie.title}</h1>
-              <p className="movie-overview">{movie.overview}</p>
-              <p className="movie-release-date">
-                Release Date: {movie.release_date}
-              </p>
-              <p className="movie-rating">Rating: {movie.vote_average}</p>
-            </div>
+      {movies.map(({ id, poster_path, title }) => (
+        <div key={id} className="movie" onClick={() => navigate(`/movie/${id}`)}>
+          <div className="image-container">
+            <img src={`https://image.tmdb.org/t/p/w500${poster_path}`} alt={title} />
           </div>
-        );
-      })}
+          <h3 className="movie-title">{title}</h3>
+        </div>
+      ))}
     </div>
   );
 };
